bafd8a35dc53d97c790d2b2f4a6c1c93acb36b94
Alan <yashikalana@mail.ru>
2026-04-10 12:48:41 +0300

    Initial commit

---
+++ AIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatAppApp.swift
+//  AIChatApp
+//
+//  Created by Алан Парастаев on 10.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct AIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
